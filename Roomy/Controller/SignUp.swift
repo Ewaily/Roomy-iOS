922721641b55d568c6853db9f9b3795010e36671
Muhammad Ewaily <muhammad.ewaily@gmail.com>
2019-07-19 13:27:20 +0200

    Adding SignUp scene

---
+++ Roomy/Controller/SignUp.swift
@@ -0,0 +1,30 @@
+//
+//  SignUp.swift
+//  Roomy
+//
+//  Created by Muhammad Ewaily on 7/19/19.
+//  Copyright © 2019 Muhammad Ewaily. All rights reserved.
+//
+
+import UIKit
+
+class SignUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
