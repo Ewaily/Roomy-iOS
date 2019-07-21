bd814c5eda5462c2f47d898a3a881e17427c5e6c
Muhammad Ewaily <muhammad.ewaily@gmail.com>
2019-07-21 23:55:10 +0200

    Retriving the rooms from the endpoint

---
+++ Roomy/Model/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Roomy
+//
+//  Created by Muhammad Ewaily on 7/21/19.
+//  Copyright © 2019 Muhammad Ewaily. All rights reserved.
+//
+
+import Foundation
---
+++ Roomy/Network/API/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  Roomy
+//
+//  Created by Muhammad Ewaily on 7/21/19.
+//  Copyright © 2019 Muhammad Ewaily. All rights reserved.
+//
+
+import UIKit
+
+class API: NSObject {
+
+}
